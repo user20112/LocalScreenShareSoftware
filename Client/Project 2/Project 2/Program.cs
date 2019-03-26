2749612f380783f5ea97e3666f655fb558be1168
user20112 <devlinpaddock@gmail.com>
2019-03-26 15:34:06 -0400

    Initial Commit
    Made this over break. wasnt using github to manage it as it was meant to be a small project so its all going in one massive commit.
    
    Client runs in the background taking a picture of the dekstop and sending it to server encoded. server decodes and updates display.

---
+++ Client/Project 2/Project 2/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Timers;
+using System.Drawing.Drawing2D;
+
+namespace Project_2
+{
+    class Program
+    {
+        static System.Timers.Timer LoopTimer;
+        public static IPAddress ipaddress = Dns.Resolve("0.0.0.0").AddressList[0];
+        public static IPEndPoint server = new IPEndPoint(ipaddress, 60001);
+        public static IPEndPoint sender = new IPEndPoint(IPAddress.Parse(GetLocalIPAddress()), 60002);
+        public static UdpClient udpSender = new UdpClient(sender);
+        public static bool connected;
+        public static bool disconnect = false;
+        public static TcpClient Tcpclient = new TcpClient();
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    if (ip.ToString()[0] == '6' && ip.ToString()[1] == '4')
+                    {
+                        return ip.ToString();
+                    }
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        static void Broadcast(string Message)
+        {
+            // string to byte array 
+            byte[] message = Encoding.ASCII.GetBytes(Message);
+
+            // broadcast to all computers on port 60001
+            udpSender.Send(message, message.Length, new IPEndPoint(IPAddress.Broadcast, 60001));
+        }
+
+        static void DataReceived(IAsyncResult ar)
+        {
+            UdpClient c = (UdpClient)ar.AsyncState;
+            IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+
+            // Convert data to ASCII and print in console
+            string receivedText = ASCIIEncoding.ASCII.GetString(receivedBytes);
+            // do this per package
+            if (receivedText == "GotIt") //thisisthefirstbroadcast
+            {
+                server.Address = receivedIpEndPoint.Address;
+                Console.WriteLine("RECIEVED");
+            }
+            else if (receivedText == "YourLive")
+            {
+                connected = true;
+            }
+            else if (receivedText == "YourDown")
+            {
+                disconnect = true;
+            }
+            c.BeginReceive(DataReceived, ar.AsyncState);
+        }
+
+        static void Disconnect(string messagetoSend)
+        {
+            // string to byte array 
+            byte[] message = Encoding.ASCII.GetBytes(messagetoSend);
+
+            // broadcast to all computers on port 60001
+            udpSender.Send(message, message.Length, server);
+            connected = false;
+        }
+        public static Bitmap ResizeImage(Bitmap image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            return destImage;
+        }
+        static Bitmap screenShot()
+        {
+            int width = Screen.PrimaryScreen.Bounds.Width;
+            int height = Screen.PrimaryScreen.Bounds.Height;
+            Bitmap bmp = new Bitmap(width, height);
+            Graphics memoryGraphics = Graphics.FromImage(bmp);
+            memoryGraphics.CopyFromScreen(0, 0, 0, 0, new Size(width, height));
+            return ResizeImage(bmp, bmp.Width, bmp.Height);//USE THIS IF YOU NEED TO CHANGE SCALING RESOLUTION/RESTRICT NUMBER OF BYTES PASSING.
+        }
+
+        static byte[] ImageToByte(System.Drawing.Image iImage)
+        {
+            MemoryStream mMemoryStream = new MemoryStream();
+            iImage.Save(mMemoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return mMemoryStream.ToArray();
+        }
+
+        static byte[] bstreamChange(byte[] bStream, byte[] sizeStream)
+        {
+            byte[] temp = new byte[bStream.Length + 4];
+
+            for (int i = 0; i < bStream.Length + 4; i++)
+            {
+                if (i < 4)
+                {
+                    temp[i] = sizeStream[i];
+                }
+                else
+                {
+                    temp[i] = bStream[i - 4];
+                }
+            }
+            return temp;
+        }
+
+        private static void LoopTimeEvent(object source, ElapsedEventArgs e)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            connected = false;
+            Broadcast("Hi!");
+
+            UdpClient receiver = new UdpClient(60003);
+            receiver.BeginReceive(DataReceived, receiver);
+
+            while (!connected)
+            {
+
+            }
+
+            Tcpclient.Connect(server.Address, 60002);
+            while (connected)
+            {
+                while (!Tcpclient.Connected)
+                {
+                    //Stalls untill server wants to connect
+                }
+                if (Tcpclient.Connected)
+                {
+                    //Takes the screenshot and puts it into bytes
+                    byte[] bStream = ImageToByte(screenShot());
+                    //Gets the size of the screenshot and puts in into bytes
+                    byte[] intStream = BitConverter.GetBytes(bStream.Length);
+                    //sets up the stream
+                    NetworkStream nstream = Tcpclient.GetStream();
+
+                    byte[] combinedStream = bstreamChange(bStream, intStream);
+
+                    //Sends it out
+                    nstream.Write(combinedStream, 0, combinedStream.Length);
+                }
+                System.Threading.Thread.Sleep(25);
+            }
+
+            Disconnect("Goodbye");
+
+            while (!disconnect)
+            {
+
+            }
+        }
+    }
+}
---
+++ Server/Project2/MainWindow.xaml.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace Project2
+{
+    /// <summary>
+    /// Shares screens 
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+        int SwitchedYet = 0;
+        int receiverPort = 60001; //thisisthe recieving port
+        static int TCPPort = 60002; //this port is used for recieveing on tdp
+        int SendPort = 60003; //this is the send port
+        IPEndPoint[] ClientIPs;
+        int size = 0;
+        IPEndPoint Client;
+        //instantiate blank client endpoint.
+        UdpClient receiver;
+        Thread FrameThread; // FrameThread
+        Thread UDPReceiverThread; //UdpRecieveThread
+        UdpClient server = new UdpClient(60003);
+        TcpListener TCPServer = new TcpListener(TCPPort);
+        static TcpClient client = new TcpClient();
+        // create a udp client. and start recieving on that port
+        string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    if (ip.ToString()[0] == '6' && ip.ToString()[1] == '4')
+                    {
+                        return ip.ToString();
+                    }
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+        void UDPRecieve()//caled by thread to recieve udp
+        {
+            receiver = new UdpClient(receiverPort);
+            receiver.BeginReceive(DataReceived, receiver);
+        }
+        public MainWindow()
+        {
+            IPEndPoint ServerTCP;
+            IPEndPoint Server;
+            IPAddress ipAddress; //used for ipaddress storage
+            string Serverip;
+            InitializeComponent();
+            this.Closed += new EventHandler(onclose);
+            FrameThread = new Thread(new ThreadStart(UpdateOneFrame));
+            UDPReceiverThread = new Thread(new ThreadStart(UDPRecieve));
+            //instantiate screen
+            Serverip = GetLocalIPAddress(); //gets the ip address
+            ipAddress = Dns.Resolve(Serverip).AddressList[0]; //obsoletebutworks
+            Server = new IPEndPoint(ipAddress, receiverPort);
+            ServerTCP = new IPEndPoint(ipAddress, TCPPort);
+            ClientIPs = new IPEndPoint[] { Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server, Server };
+            Client = new IPEndPoint(ipAddress, SendPort);//^to store all random clients max 30
+            Display("Starting Upd receiving on port: " + receiverPort);
+            Display("Server Ip: " + Serverip); // display to under left panel
+            UDPReceiverThread.Start();
+        } // end ctor
+        IPAddress localAddr = IPAddress.Parse("0.0.0.0");//the local address
+        void onclose(object sender,EventArgs e)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                Client.Address = ClientIPs[x].Address;
+                server.Send(Encoding.ASCII.GetBytes("YourDown"), 8, Client);
+            }
+            UDPReceiverThread.Abort();
+            FrameThread.Abort();
+        }
+        void SetupConnection() //sets up connection with tcp
+        {
+            TCPServer.Start();
+            bool x = true;
+            while (x)
+            {
+                if (TCPServer.Pending()) //wait for a pending connection
+                {
+                    client = TCPServer.AcceptTcpClient();
+
+                    x = false;
+                }
+            }
+        }
+        void CloseConnection()
+        {
+            // Shutdown and end connection
+            Thread.Sleep(100);
+            try
+            {
+                client.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                TCPServer.Stop();
+            }
+        }
+        byte[] RecieveAFrame() //recieves 1 frame in bytearray 
+        {
+            Byte[] Size = new Byte[4];// ends up containing size
+            int SIZE;
+            NetworkStream stream = client.GetStream();
+            Byte[] bytes;
+            int numBytesToRead = 4;
+            int numBytesRead = 0;
+            do
+            {
+                // Read may return anything from 0 to 4
+                int n = stream.Read(Size, numBytesRead, 4);
+                numBytesRead += n;
+                numBytesToRead -= n;
+            } while (numBytesToRead > 0);
+            SIZE = BitConverter.ToInt32(Size, 0);// converts byte[4] to int32
+            bytes = new Byte[SIZE];
+            numBytesToRead = SIZE;
+            numBytesRead = 0;
+            do
+            {
+                //Read may return anything from 0 to size
+                int n = stream.Read(bytes, numBytesRead, numBytesToRead);
+                numBytesRead += n;
+                numBytesToRead -= n;
+            } while (numBytesRead < SIZE);
+
+            return bytes;
+        }
+        Bitmap BytesToBitmap(byte[] bytes, Bitmap bmp)
+        {
+            using (var ms = new MemoryStream(bytes))
+            {
+                bmp = new Bitmap(ms);
+                return bmp;
+            }
+        }
+        void UpdateFromBitmap(Bitmap bmp)
+        {
+            // use invoke to cause other thread to grab it 
+            this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (Action)(() =>
+            {
+                DisplayPanel.Source = ToBitmapSource(bmp);
+                DisplayPanel.Stretch = System.Windows.Media.Stretch.Fill;
+                DisplayPanel.Visibility = Visibility.Visible;
+                bmp.Dispose();
+            }));
+        }
+        BitmapSource ToBitmapSource(Bitmap source)// convert bitmap to bitmap source
+        {
+            IntPtr hBitmap = source.GetHbitmap();
+            BitmapSource result = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, System.Windows.Int32Rect.Empty, System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            source.Dispose();
+            DeleteObject(hBitmap);
+            return result;
+        }
+        void DataReceived(IAsyncResult ar)// async data recieve ( used for udp)
+        {
+            UdpClient c = (UdpClient)ar.AsyncState;
+            IPEndPoint receivedIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Byte[] receivedBytes = c.EndReceive(ar, ref receivedIpEndPoint);
+            // Convert data to ASCII and print in console
+            string receivedText = ASCIIEncoding.ASCII.GetString(receivedBytes);
+            // do this per package
+            if (receivedText == "Hi!") //thisisthefirstbroadcast
+            {
+                Thread.Sleep(100);
+                Display(receivedText);
+                Display("Recieved First Broadcast from" + receivedIpEndPoint);
+                Client.Address = receivedIpEndPoint.Address;
+                    server.Send(Encoding.ASCII.GetBytes("GotIt"), 5, Client);
+
+                ClientIPs[size++] = Client;
+                Update();
+            }
+            else
+            {
+                if (receivedText == "GoodBye")
+                {
+                    Thread.Sleep(100);
+                    FrameThread.Abort();
+                    FrameThread = new Thread(new ThreadStart(UpdateOneFrame));
+                    CloseConnection();
+                    Remove(GetLastSegment(receivedIpEndPoint.Address.ToString()));
+                    size--;
+                    Display(receivedIpEndPoint.Address.ToString() + "shutdown");
+                    Update();
+                }
+            }
+            c.BeginReceive(DataReceived, ar.AsyncState);
+        }
+        string GetLastSegment(string ip)//gets last 3 digits for removeing people when they leave
+        {
+            int mlem = 0;
+            string TEMP = "";
+            for (int w = 0; w < ip.Length; w++)
+            {
+                if (ip[w] == '.')//10.0.0.17
+                {
+                    mlem++;
+                }
+                if (mlem == 3)
+                {
+                    TEMP += ip[w];
+                }
+            }
+            string TEmp = "";
+            for (int x = 1; x < TEMP.Length; x++)
+            {
+                TEmp += TEMP[x];
+            }
+            return TEmp;
+        }
+        void Update()//updates left panel 
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                if (size == 0)
+                {
+                }
+                else
+                {
+                    Button Entry = new Button();
+                    Entry.Name = "name" + GetLastSegment(ClientIPs[size].Address.ToString());//+ClientIPs[size].Address.ToString();
+                    Entry.Width = 25;
+                    Entry.Height = 10;
+                    Entry.FontSize = 3;
+                    Entry.Margin = new Thickness(5);
+                    Entry.Content = ClientIPs[size - 1].Address.ToString();
+                    Entry.HorizontalAlignment = HorizontalAlignment.Left;
+                    Entry.HorizontalContentAlignment = HorizontalAlignment.Left;
+                    Entry.VerticalAlignment = VerticalAlignment.Top;
+                    Entry.Click += (sender, args) =>
+                    {
+                        Switch(Entry.Content.ToString());
+                    };
+                    LeftPanel.Children.Add(Entry);
+                }
+            });
+        }
+        void Remove(String name) //removes a child from left panel
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var Tempymctemp = (Button)this.FindName("name" + name);
+                LeftPanel.Children.Remove(Tempymctemp);
+            });
+        }
+        void Switch(String IP) //this is called on button click
+        {
+            if (SwitchedYet == 1)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    Client.Address = ClientIPs[x].Address;
+                        server.Send(Encoding.ASCII.GetBytes("YourDown"), 8, Client);
+                }
+                if (FrameThread.IsAlive)
+                {
+                    FrameThread.Abort();
+                }
+
+                FrameThread = new Thread(new ThreadStart(UpdateOneFrame));
+                CloseConnection();
+            }
+            Client.Address = IPAddress.Parse(IP);
+            //Client.Address = Dns.Resolve(IP).AddressList[0];
+            server.Send(Encoding.ASCII.GetBytes("YourLive"), 8, Client);
+            SetupConnection();
+            SwitchedYet = 1;
+
+            FrameThread.Start();
+        }
+        void Display(string content)//this displays in the textbox under leftpanel
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                TextBox.FontSize = 6;
+                TextBox.Text += System.Environment.NewLine;
+                TextBox.Text += (content);
+            });
+        }
+        void UpdateOneFrame()//1 frame worth of logic
+        {
+            while (true)
+            {
+                using (Bitmap bmp = null)
+                {
+                    UpdateFromBitmap(BytesToBitmap(RecieveAFrame(), bmp));
+                    //Thread.Sleep(25);
+                }
+            }
+        }
+    }
+}
